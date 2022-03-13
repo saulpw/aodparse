c1048c4fd424de91996bacfd13c73236eb2d8840
pesk <pesk@circe.local>
2022-03-13 16:50:35 -0700

    save groups of frames as animated pngs

--- vdres.py
+++ vdres.py
@@ -1,7 +1,8 @@
 import struct
 import parse95
 
-from visidata import VisiData, Sheet, AttrColumn, AttrDict, vd
+from PIL import Image
+from visidata import VisiData, Sheet, AttrColumn, AttrDict, vd, ENTER
 
 
 @VisiData.api
@@ -23,6 +24,12 @@ class ResSheet(Sheet):
     def openRow(self, row):
         return FramesSheet(row.index, source=row)
 
+    def openRows(self, rows):
+        frame_rows = []
+        for row in rows:
+            frame_rows.extend(row.iter_frames())
+        return FramesSheet("many rows", rows=frame_rows)
+
 class FramesSheet(Sheet):
     rowtype = 'frames'  # rowdef: parse95.Frame
     columns = [
@@ -35,3 +42,15 @@ class FramesSheet(Sheet):
     ]
     def iterload(self):
         yield from self.source.iter_frames()
+
+@VisiData.api
+def save_frames(vd, p, rows):
+    frames = []
+    for row in rows:
+        im = Image.new("L", (row.w, row.h))
+        im.putdata(b''.join(row.byte_rows))
+        frames.append(im)
+    frames[0].save(str(p), append_images=frames[1:], optimize=False, save_all=True, duration=100, loop=0)
+
+ResSheet.addCommand('g'+ENTER, "dive-selected", "vd.push(sheet.openRows(selectedRows))")
+FramesSheet.addCommand("X", "save-frame", "vd.save_frames(Path('foo.png'), rows)")
