c2d3233c295c656b760dee329e191642cff09845
pesk <pesk@circe.local>
2022-03-22 01:25:56 -0700

    generate html gallery for frames in selected blocks

--- parse95.py
+++ parse95.py
@@ -1,6 +1,40 @@
 import struct
 import sys
 
+# mainly based on RES001
+block_types = {
+    b'\x01\x00\x00\x00': "text(?)", # one help text block, one short 18-byte block
+    b'\x03\x00\x00\x00': "text", # misc help text
+    b'\x05\x00\x00\x00': "text", # help text for left button
+    b'\x06\x00\x00\x00': "text", # help text for left button
+    b'\x07\x00\x00\x00': "text", # help text for left button
+    b'\x0a\x00\x00\x00': "text", # help text for left button
+    b'\x0b\x00\x00\x00': "text", # help text for left button
+    b'\x0c\x00\x00\x00': "text", # misc help text
+    b'\x0f\x00\xf7\x00': "text", # misc help text
+    b'\x10\x00\x14\x01': "text", # help text for left button
+    b'\x13\x00\xf7\x00': "text", # misc help text
+    b'\x44\x33\x47\x52': "graphic",
+}
+
+# mainly based on RES001
+block_subtypes = {
+    b'\x00\x00\x00\x00': "palette",
+    b'\x20\x00\x00\x00': "image", # single-frame, mostly characters
+    b'\x22\x00\x00\x00': "selector", # player-controlled icons i think, all single-frame
+    b'\x24\x00\x00\x00': "2-frame image", # both frames are same-sized, meant to be layered/animated, or just different views?
+
+    # might be groups of animations and/or an animation with frames of different sizes that
+    # should be layered at particular relative positions
+    b'\x28\x00\x00\x00': "animation",
+    b'\x2c\x00\x00\x00': "animation",
+
+    b'\x2e\x00\x00\x00': "selectors/icons", # not sure what exactly these things are, multi-frame
+    b'\x4c\x00\x00\x00': "animated icon(s)", # usually all frames are same-sized/same icon but one block contains several icons
+    b'\x1c\x02\x00\x00': "font",
+    b'\x24\x03\x00\x00': "background", # single-frame 320x200 image
+}
+
 class Res():
     def __init__(self, filename):
         self.buf = open(filename, mode='rb').read()
@@ -16,7 +50,7 @@ class Res():
                 break
             yield block
             i += 1
-                
+
     def get_block_offset(self, blocknum):
         offset, = struct.unpack_from('<L', self.buf, (blocknum+1)*4)
         return offset
@@ -34,18 +68,25 @@ class DataBlock():
         self.index = index
         self.offset = offset
         self.buf = buf
-        self.magic = ' '.join(hex(x)[2:].zfill(2) for x in self.buf[:4])
+        self.type_id = self.buf[:4]
+        self.type_id_str = ' '.join(f'{x:02x}' for x in self.type_id)
+        self.subtype_id = self.buf[8:12]
+        self.subtype_id_str = ' '.join(f'{x:02x}' for x in self.subtype_id)
+        self.block_type = block_types.get(self.type_id, "")
+        self.block_subtype = block_subtypes.get(self.subtype_id, "")
         if len(self.buf) < 24:
             return
         self.header = struct.unpack_from('<LLLLLL', self.buf)
-        i = self.header[2] # offset of chunk 3
-        if i > len(self.buf):
-            self.chunk3_size = 0
-        else:
-            self.chunk3_size, = struct.unpack_from('<L', self.buf[i:i+4])
+        self.header_str = ' '.join(f'{x:02x}' for x in self.buf[:24])
+        self.chunk3_offset = self.header[2] # offset of chunk 3
+        self.chunk3_size = 0
+#        if self.chunk3_offset > len(self.buf):
+#            self.chunk3_size = 0
+#        else:
+#            self.chunk3_size, = struct.unpack_from('<L', self.buf[self.chunk3_offset:self.chunk3_offset+4])
 
     def __str__(self):
-        return f"Block offset: {self.offset:x} Block length: {self.size:d} {self.magic}"
+        return f"Block offset: {self.offset:x} Block length: {self.size:d}"
 
     @property
     def size(self):
@@ -69,13 +110,11 @@ class Frame():
         self.h, self.w, = struct.unpack_from('<HH', self.header[-4:])
         self.byte_rows = []
         self.rows = []
-        self.hex_rows = []
         i = 0
         while i < self.h*self.w:
             row = self.body[i:i+self.w]
             self.byte_rows.append(row)
             self.rows.append(self.asciify(row))
-#            self.hex_rows.append(' '.join(f'{c:02x}' for c in row))
             i += self.w
 
     def asciify(self, row):
@@ -96,5 +135,3 @@ if __name__ == '__main__':
     res = Res(sys.argv[1])
     for i, block in enumerate(res):
         print("Block %d: %s" % (i, block))
-#        for j, frame in enumerate(block):
-#            print("  Frame %d: %s" % (j, frame))
--- vdres.py
+++ vdres.py
@@ -14,9 +14,13 @@ class ResSheet(Sheet):
     rowtype = 'blocks'  # rowdef: parse95.DataBlock
     columns = [
         AttrColumn('index', type=int),
-        AttrColumn('offset', type=int),
+        AttrColumn('offset', type=int, fmtstr='{:x}'),
         AttrColumn('size'),
-        AttrColumn('magic'),
+        AttrColumn('block_type'),
+        AttrColumn('block_subtype'),
+        AttrColumn('type_id_str'),
+        AttrColumn('subtype_id_str'),
+        AttrColumn('header_str')
     ]
     def iterload(self):
         yield from parse95.Res(str(self.source)).iter_blocks()
@@ -43,14 +47,66 @@ class FramesSheet(Sheet):
     def iterload(self):
         yield from self.source.iter_frames()
 
+
+def make_palette():
+    palette = []
+    with open('ANVIL0.PAL', 'rb')  as f:
+        palette = list(f.read())
+    return [4*b for b in palette]
+
+def make_img(palette, frame):
+    im = Image.new("L", (frame.w, frame.h))
+    im.putpalette(palette, rawmode="RGB")
+    im.putdata(b''.join(frame.byte_rows))
+    return im
+
 @VisiData.api
 def save_frames(vd, p, rows):
+    palette = make_palette()
     frames = []
     for row in rows:
-        im = Image.new("L", (row.w, row.h))
-        im.putdata(b''.join(row.byte_rows))
-        frames.append(im)
+        frames.append(make_img(palette, row))
     frames[0].save(str(p), append_images=frames[1:], optimize=False, save_all=True, duration=100, loop=0)
 
+@VisiData.api
+def make_gallery(vd, dirname, blocks):
+    palette = make_palette()
+
+    head = "<!DOCTYPE html> <head> <link rel='stylesheet' href='gallery.css'> </head> "
+    toc = f"<h1>{vd.sheet.source} Blocks </h1> <div class='toc'> "
+    body = "<body> "
+
+    res_dir = dirname.joinpath(f"{vd.sheet.source}_resources")
+    if not res_dir.exists():
+        res_dir.mkdir()
+
+    for block in blocks:
+        body += f"<h1 id={str(block.index)}> {str(block.index)} </h1> <div class='block'> "
+        toc += f"<div class='tocitem'> <a href=#{str(block.index)}> {str(block.index)} </a> </div>"
+
+        # list non-graphic blocks in the gallery but don't attempt to parse content
+        if block.block_type != "graphic":
+            body += "</div> "
+            continue
+
+        for frame in block.iter_frames():
+            # display a broken img link for unparseable frames
+            img_path = str(res_dir.joinpath(f"{str(block.index)}_{str(frame.index)}.png"))
+            if frame.byte_rows:
+                im = make_img(palette, frame)
+                im.save(img_path)
+            body += f"<div class='frame'> <a target='_blank' href={img_path}> <img src={img_path}> </a> </div> "
+        body += "</div> "
+
+    toc += "</div> "
+    html = head + toc + body + "</body> </html>"
+    with dirname.joinpath(f'{vd.sheet.source}_gallery.html').open('w') as f:
+        f.write(html)
+
 ResSheet.addCommand('g'+ENTER, "dive-selected", "vd.push(sheet.openRows(selectedRows))")
+
+# just create the gallery in the current dir for now
+# TODO: make the path an argument
+ResSheet.addCommand("X", "make-gallery", "vd.make_gallery(Path(''), selectedRows)")
+
 FramesSheet.addCommand("X", "save-frame", "vd.save_frames(Path('foo.png'), rows)")
