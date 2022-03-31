04b0e89422330d36f490165f08c73140ca91b19c
pesk <pesk@circe.local>
2022-03-30 21:05:16 -0700

    generate sprite sheets from specified blocks

---
+++ README.md
@@ -0,0 +1,7 @@
+# Anvil of Dawn sprite sheets
+
+![RES.001](res001.png)
+![RES.005](res005.png)
+![RES.006](res006.png)
+
+Data in the other RES files was not graphically interesting.
--- parse95.py
+++ parse95.py
@@ -1,6 +1,8 @@
 import struct
 import sys
 
+from PIL import Image, ImageDraw
+
 # mainly based on RES001
 block_types = {
     b'\x01\x00\x00\x00': "text(?)", # one help text block, one short 18-byte block
@@ -139,8 +141,140 @@ class Frame():
             image += self.rows[i] + '\n'
         return image
 
+def make_palette():
+    palette = []
+    with open('ANVIL0.PAL', 'rb')  as f:
+        palette = list(f.read())
+    return [4*b for b in palette]
+
+def make_img(palette, w, h, data):
+    if w*h != len(data):
+#        print(w, h, len(data))
+        return
+    if all(x == b'\xfe' for x in data):
+        return
+    im = Image.new("L", (w, h))
+    im.putpalette(palette, rawmode="RGB")
+    im.putdata(data)
+    return im
+
+def make_mask(w, h, data):
+    if w*h != len(data):
+        return
+    im = Image.new("L", (w, h))
+    im.putdata(data)
+    return im
+
+def frames_to_imgs(frames, block_type):
+    palette = make_palette()
+    imgs = []
+#    if frames:
+#        h, w = frames[0].h, frames[0].w
+    h, w = 0, 0
+    for frame in frames:
+        if frame.h > h:
+            h = frame.h
+        if frame.w > w:
+            w = frame.w
+    for frame in frames:
+        img = make_img_frame(palette, imgs, frame, w, h, block_type)
+        if img:
+            imgs.append(img)
+    return imgs
+
+def make_img_frame(palette, imgs, frame, w, h, block_type):
+    pad = (frame.h != h or frame.w != w)
+    if not block_type.startswith('animat') or not pad:
+        return make_img(palette, frame.w, frame.h, b''.join(frame.byte_rows))
+    left = b'\x00' * frame.x
+    right = b'\x00' * (w - frame.w - frame.x)
+    top = [b'\x00' * w] * frame.y
+    bottom = [b'\x00' * w] * (h - frame.h - frame.y)
+
+    padded = [b'\x00' * w] * frame.y
+    mask = [b'\x00' * w] * frame.y
+    for row in frame.byte_rows:
+        padded.append(b''.join([left,row,right]))
+        mask.append(b''.join([b'\xff' if p else b'\x00' for p in padded[-1]]))
+    if bottom:
+        padded = padded + bottom
+        mask = mask + bottom
+
+    img = make_img(palette, w, h, b''.join(padded))
+    mask = make_mask(w, h, b''.join(mask))
+    if img and mask and imgs:
+        return Image.composite(img, imgs[-1], mask)
+    return img
+
+def make_sprite_sheet(spec, filename, outfile, w=1500, h=3000):
+    # each element of rows is a list of block indices
+    rows = []
+    with open(spec, 'r') as f:
+        rows = [list(map(int, x.split())) for x in f.readlines() if x and not x.startswith('#')]
+
+    res = Res(filename)
+    palette = make_palette()
+    maxw = 0 # computed
+    minw = 20
+    bkgds = [make_img(palette, w, h, b'\x6a'*w*h) for i in range(12)]
+    draws = [ImageDraw.Draw(bkgd) for bkgd in bkgds]
+    imgs = []
+    x, y = 0, 0
+    maxrowh = 0
+
+    for draw in draws:
+        draw.text((x+5, y), "ANVIL OF DAWN (1995)")
+        draw.text((x+5, y+12), f"numbers refer to data blocks in {filename}")
+        draw.text((x+5, y+24), "extracted by saul.pw & peskin (2022)")
+
+    y += 50
+
+    blocks = list(res.iter_blocks())
+    for row in rows:
+        y += maxrowh + 20
+        maxrowh = 0
+        x = 0
+        for blocknum in row:
+            x += 5
+            block = blocks[blocknum]
+            if block.block_type != "graphic":
+                print("block type is not 'graphic' for block", blocknum)
+                continue
+
+            imgs = frames_to_imgs(list(block.iter_frames()), block.block_subtype)
+            if imgs:
+                print(f"{blocknum}") #  {len(imgs)} imgs for block {blocknum)
+            else:
+                continue
+
+            for draw in draws:
+                draw.text((x, y-12), str(blocknum))
+
+            oldx = x
+            for i, bkgd in enumerate(bkgds):
+                img = imgs[i] if i < len(imgs) else imgs[0]
+                x = oldx
+                if x + img.width > w:
+                    print("running off the right edge for block", blocknum)
+                    break
+                if block.block_subtype.startswith('animat'):
+                    bkgd.paste(img, (x, y))
+                else:
+                    for img in imgs:
+                        bkgd.paste(img, (x, y))
+                        x += max(img.width + 5, minw)
+                        maxrowh = max(maxrowh, img.height)
+            if block.block_subtype.startswith('animat'):
+                x += max(img.width + 5, minw)
+                maxrowh = max(maxrowh, img.height)
+                print(f"{len(imgs)} frames in animation block {blocknum}", file=sys.stderr)
+            maxw = max(maxw, x)
+    bkgds[0].save(outfile, append_images=bkgds[1:], optimize=False, save_all=True, duration=150, loop=0)
+    return maxw, y + maxrowh + 20
 
 if __name__ == '__main__':
-    res = Res(sys.argv[1])
-    for i, block in enumerate(res):
-        print("Block %d: %s" % (i, block))
+#    res = Res(sys.argv[1])
+#    for i, block in enumerate(res):
+#        print("Block %d: %s" % (i, block))
+    w, h = make_sprite_sheet(*sys.argv[1:])
+    make_sprite_sheet(*sys.argv[1:], w, h)
